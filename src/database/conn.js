c3be56a16f3696099cb758493104b8980e3dc735
kayke hartmann <kaykeharmann3000@gmail.com>
2024-09-19 09:51:14 -0300

    init: atualização de jogos

--- src/database/conn.js
+++ src/database/conn.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
-const mongoDB =
-  "mongodb+srv://jleachenski:ISSIN64KOwheXth2@cluster0.osw4i.mongodb.net/";
+const mongoDB ="mongodb+srv://kaykeharmann:kayke290616@cluster0.lm7gq.mongodb.net/"
+
 const database = "lionsbet";
 
 const main = async () => {
   try {
-    await mongoose.connect(mongoDB + database);
+    await mongoose.connect(process.env.MONGODB + process.env.DATABASE);
     console.log("Conectado com sucesso");
   } catch (error) {
     console.log(error);
